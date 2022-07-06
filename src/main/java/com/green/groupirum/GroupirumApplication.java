3d7fe986c62117b9cb8828364c2cc20008b5260e
pt807 <pt807@naver.com>
2022-07-06 22:15:08 +0900

    Initial commit

---
+++ src/main/java/com/green/groupirum/GroupirumApplication.java
@@ -0,0 +1,13 @@
+package com.green.groupirum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupirumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupirumApplication.class, args);
+	}
+
+}
