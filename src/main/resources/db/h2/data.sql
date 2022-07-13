ea6b2c50823f4f07cb481c855a69fe3b276008dc
hunbk <shyun823@gmail.com>
2022-07-13 17:32:22 +0900

    feat: H2 DB 더미 데이터 추가 #9

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO game VALUES (default, '오버워치');
+INSERT INTO game VALUES (default, '리그오브레전드');
+INSERT INTO game VALUES (default, '배틀그라운드');
+
+INSERT INTO member VALUES (default, 'gamer1@gamer.com', '게이머1');
+INSERT INTO member VALUES (default, 'gamer2@gamer.com', '게이머2');
+INSERT INTO member VALUES (default, 'gamer3@gamer.com', '게이머3');
+INSERT INTO member VALUES (default, 'gamer4@gamer.com', '게이머4');
+INSERT INTO member VALUES (default, 'gamer5@gamer.com', '게이머5');
+
+INSERT INTO recruit VALUES (default, '탱커 장인 구함', '안녕하세요. 오버워치 탱커 장인 1명 구합니다.', 1, 'discord.com', 'HIRING', default, now(), now(), 1, 1);
+INSERT INTO recruit VALUES (default, '탑 구함', '안녕하세요. 다이아 이상 탑 라이너 구합니다.', 1, 'discord.com', 'HIRING', default, now(), now(), 2, 2);
+INSERT INTO recruit VALUES (default, '정글 구함', '안녕하세요. 다이아 이상 정글러 구합니다.', 1, 'discord.com', 'HIRING', default, now(), now(), 2, 2);
+INSERT INTO recruit VALUES (default, '배그 스쿼드 같이 하실분?', '같이 배그 스쿼드 하실분 3명 구함.', 3, 'discord.com', 'HIRING', default, now(), now(), 3, 3);
+INSERT INTO recruit VALUES (default, '오버워치 ㄱㄱ', '5명 선착순', 5, 'discord.com', 'HIRING', default, now(), now(), 1, 4);
+INSERT INTO recruit VALUES (default, '칼바람 구함', '칼바람 4명 구해요.', 4, 'discord.com', 'HIRING', default, now(), now(), 2, 5);
