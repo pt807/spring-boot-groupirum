d5a66278ed1668cd6ff2fa216ea5d84e1477b4dc
hunbk <shyun823@gmail.com>
2022-07-13 17:00:04 +0900

    feat: Entity 수정 #10

--- src/main/java/com/green/groupirum/domain/Member.java
+++ src/main/java/com/green/groupirum/domain/Member.java
@@ -3,9 +3,9 @@ package com.green.groupirum.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
-import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -18,13 +18,10 @@ public class Member {
     private Long id;
 
     @Column(nullable = false, length = 8)
-    @Size(min = 2, max = 8)
+    @Length(min = 2, max = 8)
     private String nickname;
 
     @Column(nullable = false, length = 50)
     private String email;
-
-    @Column(nullable = false, length = 20)
-    private String password;
 
 }
