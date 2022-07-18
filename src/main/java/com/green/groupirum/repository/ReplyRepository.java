0d497c2a895c26b735a552e59e72c68256decbb2
pt807 <pt807@naver.com>
2022-07-18 19:51:30 +0900

    feat: ReplyService 생성, view.js 생성, ReplyRepository 수정 #17

--- src/main/java/com/green/groupirum/repository/ReplyRepository.java
+++ src/main/java/com/green/groupirum/repository/ReplyRepository.java
@@ -3,5 +3,8 @@ package com.green.groupirum.repository;
 import com.green.groupirum.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findByRecruitIdAndParentIsNullOrderByIdDesc(Long recruitId);
 }
---
+++ src/main/java/com/green/groupirum/service/ReplyService.java
@@ -0,0 +1,15 @@
+package com.green.groupirum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReplyService {
+
+    private final ReplyService replyService;
+
+
+}
