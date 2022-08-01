70e06a408ba0f9af25bfa52d56e6f764763c003d
pt807 <pt807@naver.com>
2022-08-01 21:27:06 +0900

    feat: contact_address, recruit_date 데이터 추가

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -7,13 +7,14 @@ INSERT INTO member VALUES (default, 'gamer2@gamer.com', '게이머2', 'google000
 INSERT INTO member VALUES (default, 'gamer3@gamer.com', '게이머3', 'google000003', '1002', 'google', 'USER');
 INSERT INTO member VALUES (default, 'gamer4@gamer.com', '게이머4', 'google000004', '1003', 'google', 'USER');
 INSERT INTO member VALUES (default, 'gamer5@gamer.com', '게이머5', 'google000005', '1004', 'google', 'USER');
+INSERT INTO member VALUES (default, 'pt807@naver.com', 'test', 'kakao000006', '1005', 'kakao', 'USER');
 
-INSERT INTO recruit VALUES (default, '탱커 장인 구함', '안녕하세요. 오버워치 탱커 장인 1명 구합니다.', 1, 'discord.com', 'HIRING', default, now(), now(), 1, 1);
-INSERT INTO recruit VALUES (default, '탑 구함', '안녕하세요. 다이아 이상 탑 라이너 구합니다.', 1, 'discord.com', 'HIRING', default, now(), now(), 2, 2);
-INSERT INTO recruit VALUES (default, '정글 구함', '안녕하세요. 다이아 이상 정글러 구합니다.', 1, 'discord.com', 'HIRING', default, now(), now(), 2, 2);
-INSERT INTO recruit VALUES (default, '배그 스쿼드 같이 하실분?', '같이 배그 스쿼드 하실분 3명 구함.', 3, 'discord.com', 'HIRING', default, now(), now(), 3, 3);
-INSERT INTO recruit VALUES (default, '오버워치 ㄱㄱ', '5명 선착순', 5, 'discord.com', 'HIRING', default, now(), now(), 1, 4);
-INSERT INTO recruit VALUES (default, '칼바람 구함', '칼바람 4명 구해요.', 4, 'discord.com', 'HIRING', default, now(), now(), 2, 5);
+INSERT INTO recruit VALUES (default, '탱커 장인 구함', '안녕하세요. 오버워치 탱커 장인 1명 구합니다.', 1, 'discord.com', 'https://discord.com','HIRING', default, '2022-08-03',now(), now(), 1, 1);
+INSERT INTO recruit VALUES (default, '탑 구함', '안녕하세요. 다이아 이상 탑 라이너 구합니다.', 1, 'discord.com', 'https://discord.com','HIRING', default, '2022-08-03',now(), now(), 2, 2);
+INSERT INTO recruit VALUES (default, '정글 구함', '안녕하세요. 다이아 이상 정글러 구합니다.', 1, 'discord.com', 'https://discord.com','HIRING', default, '2022-08-03',now(), now(), 2, 2);
+INSERT INTO recruit VALUES (default, '배그 스쿼드 같이 하실분?', '같이 배그 스쿼드 하실분 3명 구함.', 3, 'discord.com', 'https://discord.com','HIRING', default, '2022-08-03',now(), now(), 3, 3);
+INSERT INTO recruit VALUES (default, '오버워치 ㄱㄱ', '5명 선착순', 5, 'discord.com', 'https://discord.com','HIRING', default, '2022-08-03',now(), now(), 1, 4);
+INSERT INTO recruit VALUES (default, '칼바람 구함', '칼바람 4명 구해요.', 4, 'discord.com', 'https://discord.com','HIRING', default, '2022-08-03',now(), now(), 2, 6);
 
 INSERT INTO reply VALUES (default, now(), now(), '혹시 힐러 유저인데 같이 가능 할까요?', 2, null, 1);
 INSERT INTO reply VALUES (default, now(), now(), '지금 자리 남아있나요?', 3, null, 1);
